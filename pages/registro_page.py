1de2f45f778911c3d5add0e8c1b02e9c429d1157
Miguel Ramos <return.infox@gmail.com>
2025-11-03 14:23:50 -0300

    codigo de registro

--- pages/registro_page.py
+++ pages/registro_page.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
 class RegistroPage(BasePage):
+    # Localizadores 
     USERNAME = (By.ID, "username")
     PASSWORD = (By.ID, "password")
     EMAIL = (By.ID, "email")
@@ -18,5 +19,16 @@ class RegistroPage(BasePage):
 
     def submit(self):
         self.driver.find_element(*self.BTN_SUBMIT).click()
+
+    def feedback_text(self)-> str:
+        return self.wait_present(self.FEEDBACK).text
+    
+    def has_success_class(self) -> bool:
+        classes = self.wait_present(self.FEEDBACK).get_attribute("class")
+        return "mensaje-exito" in classes.split()
+    
+    def has_error_class(self) -> bool:
+        classes = self.wait_present(self.FEEDBACK).get_attribute("class")
+        return "mensaje-error" in classes.split()
 
         
