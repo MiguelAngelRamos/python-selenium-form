a38d97a2b09fbc33c33b20c2e1d6717ea1ba3ea7
Miguel Ramos <return.infox@gmail.com>
2025-11-03 14:42:35 -0300

    codigo actualizado

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import os
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture(scope="session")
+def base_url():
+    return os.getenv("BASE_URL", "https://web-form-selenium.netlify.app/")
+
+def driver():
+    service = ChromeService(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+@pytest.fixture(autouse=True)
+def reset_between_test(driver, base_url):
+    driver.delete_all_cookies()
+    driver.get(f"{base_url}/index.html")
+    yield
