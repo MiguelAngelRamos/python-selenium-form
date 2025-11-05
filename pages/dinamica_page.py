1016499ce19166cced644af075894e667ca3a685
Miguel Ramos <return.infox@gmail.com>
2025-11-05 14:12:42 -0300

    dinamica

--- pages/dinamica_page.py
+++ pages/dinamica_page.py
@@ -1,28 +1,27 @@
-from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from .base_page import BasePage
+
 
 class DinamicaPage(BasePage):
-    # Localizadores | btn-alerta | btn-mostrar-delay | mensaje-oculto | link-google
-
+    # Localizadores
     BTN_ALERTA = (By.ID, "btn-alerta")
     BTN_DELAY = (By.ID, "btn-mostrar-delay")
     MSG_OCULTO = (By.ID, "mensaje-oculto")
-    LINK_GOOGLE = (By.ID, "link-google" )
+    LINK_GOOGLE = (By.ID, "link-google")
 
-    ## Trigger
-    def trigger_alert(self):
-        self.wait_present(self.BTN_ALERTA).click()
-    
-    def trigger_delay(self):
-        self.wait_present(self.BTN_DELAY).click()
+    def trigger_alert(self) -> None:
+        """Dispara la alerta del botón."""
+        self.safe_click(self.BTN_ALERTA)
 
-    
+    def trigger_delay(self) -> None:
+        """Hace click en el botón que luego muestra el mensaje con retardo."""
+        self.safe_click(self.BTN_DELAY)
+
     def delayed_message_text(self) -> str:
-        element = self.wait_visible(self.MSG_OCULTO)
-        return element.text
-    
-    def click_external_link(self):
-        self.wait_present(self.LINK_GOOGLE).click()
-    
+        """Devuelve el texto del mensaje que aparece tras el delay."""
+        elem = self.wait_visible(self.MSG_OCULTO)
+        return elem.text
 
-
+    def click_external_link(self) -> None:
+        """Abre el link externo (target=_blank)."""
+        self.safe_click(self.LINK_GOOGLE)
